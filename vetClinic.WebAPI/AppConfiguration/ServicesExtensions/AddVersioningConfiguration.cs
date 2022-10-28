f479315444d7fa4f2708c073146658b4e356b2a3
offmu <offmurphy@yandex.ru>
2022-10-28 19:12:27 +0300

    task 2

---
+++ vetClinic.WebAPI/AppConfiguration/ServicesExtensions/AddVersioningConfiguration.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace vetClinic.WebAPI.AppConfiguration.ServicesExtensions
+{
+
+    public static partial class ServicesExtensions
+    {
+        /// <summary>
+        /// Configure versioning
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddVersioningConfiguration(this IServiceCollection services)
+        {
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+    }
+
+}
--- vetClinic.WebAPI/Program.cs
+++ vetClinic.WebAPI/Program.cs
@@ -1,19 +1,26 @@
+using vetClinic.WebAPI.AppConfiguration.ServicesExtensions;
+using vetClinic.WebAPI.AppConfiguration.ApplicationExtensions;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.AddSerilogConfiguration();
+builder.Services.AddVersioningConfiguration(); //add api versioning
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfiguration();
+
+
+
 
 var app = builder.Build();
+
+app.UseSerilogConfiguration();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerConfiguration();
 }
 
 app.UseHttpsRedirection();
